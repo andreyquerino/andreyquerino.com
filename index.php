7b6867bbd21fc44a22f78344952bb50da1c68bb8
Andrey <88719652+andreyquerino@users.noreply.github.com>
2021-12-28 18:00:13 -0300

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php
-
-define( 'WP_USE_THEMES', true );
-
-require __DIR__ . '/andrey.html';
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <title>Andrey Querino</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:site_name" content="andreyquerino.com" />
+  <meta property="og:title" content="Andrey Cardoso Querino" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://andreyquerino.com" />
+  <link rel="apple-touch-icon" sizes="180x180" href="https://andreyquerino.com/img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://andreyquerino.com/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://andreyquerino.com/img/favicon-16x16.png">
+  <link rel="manifest" href="https://andreyquerino.com/img/site.webmanifest">
+  <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+  <script src="https://andreyquerino.com/js/fontawesome.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="user-bar">
+    <a title="Voltar uma página" onclick="window.close();"><div class="back"><i class="fas fa-arrow-left"></i></div></a>
+    <div class="avatar"><img src="img/avatar.jpg" alt="Avatar"></div>
+    <div class="name"><span>Andrey</span><span class="status">online</span></div>
+    <div class="actions more" onclick="window.close();"><i class="fas fa-ellipsis-v"></i></div>
+  </div>
+  <div class="messages"></div>
+  <script src="js/anime.js"></script>
+  <script src="js/scripts.js"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SS7X747R0G"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SS7X747R0G');
+  </script>
+</body>
+</html>
